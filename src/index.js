8932cf954c74ecf5f7062b29a29895ad9839a9a3
castro-d <castro.dh91@gmail.com>
2022-07-12 19:14:04 -0300

    add post characters route

--- src/index.js
+++ src/index.js
@@ -24,8 +24,47 @@ Pelicula.setupAssociation(Personaje);
 Personaje.setupAssociation(Pelicula);
 Genero.setupAssociation(Pelicula);
 
-app.get('/characters', (req, res) => {
-  res.send('Hello World!');
+app.use(express.json());
+
+app.get('/characters', async (req, res) => {
+  const personajes = await Personaje.findAll({
+    attributes: ['imagen', 'nombre'],
+  });
+  res.status(200).json(personajes);
+});
+
+app.get('/characters/:id', async (req, res) => {
+  const { id } = req.params;
+  const personaje = await Personaje.findByPk(id, {
+    include: {
+      model: Pelicula,
+      as: 'peliculas',
+      through: 'peliculas_personajes',
+    },
+  });
+  res.status(200).json({ data: personaje });
+});
+
+app.post('/characters', async (req, res) => {
+  const personaje = req.body;
+  const { peliculas } = personaje;
+  const newPersonaje = Personaje.build(personaje);
+  await newPersonaje.save();
+  if (peliculas) {
+    peliculas.forEach(async (p) => {
+      const pelicula = await Pelicula.findOne({
+        where: {
+          titulo: p.titulo,
+        },
+      });
+      const peliculaPersonaje = PeliculaPersonaje.build({
+        fk_pelicula: pelicula.id,
+        fk_personaje: newPersonaje.id,
+      });
+      await peliculaPersonaje.save();
+    });
+  }
+  res.status(200).json(newPersonaje.toJSON());
 });
 
 app.listen(port, () => {
