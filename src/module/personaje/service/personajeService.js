6db6d9ab01bed471131f1716cf1a68bc15df4b76
castro-d <castro.dh91@gmail.com>
2022-07-24 08:46:04 -0300

    add service to characters

---
+++ src/module/personaje/service/personajeService.js
@@ -0,0 +1,32 @@
+module.exports = class personajeService {
+  /**
+   * @param {import('../repository/personajeRepository')} personajeRepository
+   */
+  constructor(personajeRepository) {
+    this.personajeRepository = personajeRepository;
+  }
+
+  async getAll() {
+    return this.personajeRepository.getAll();
+  }
+
+  async getFilteredCharacters(query, pelicula) {
+    return this.personajeRepository.getFilteredCharacters(query, pelicula);
+  }
+
+  async getCharacterById(id) {
+    return this.personajeRepository.getCharacterById(id);
+  }
+
+  async save(data) {
+    return this.personajeRepository.save(data);
+  }
+
+  async saveCharactersMovies(peliculas, personaje) {
+    return this.personajeRepository.saveCharactersMovies(peliculas, personaje);
+  }
+
+  async removeCharacter(id) {
+    return this.personajeRepository.removeCharacter(id);
+  }
+};
