ac59f1dafba5f7d9b12cc923d1e568c8056d4f87
castro-d <castro.dh91@gmail.com>
2022-08-07 16:19:38 -0300

    test uri

--- src/cli/config.js
+++ src/cli/config.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
-    host: '127.0.0.1',
-    port: 5432,
     dialect: 'postgres',
     dialectOptions: {
       bigNumberStrings: true,
